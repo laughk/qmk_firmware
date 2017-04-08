90241c9b9ad54a19fff822f41e431233e969784c
Kei Iwasaki <me@laughk.org>
2017-04-08 17:28:33 +0900

    update keybind

--- keyboards/ergodox/keymaps/laughk/keymap.c
+++ keyboards/ergodox/keymaps/laughk/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | ESC    |   1  |   2  |   3  |   4  |   5  |  `   |           |   6  |   7  |   8  |   9  |   0  |  -   |   =    |
+ * | ESC    |   1  |   2  |   3  |   4  |   5  |   6  |           |   7  |   8  |   9  |   0  |  -   |   =  | Backsp |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | TAB    |   Q  |   W  |   E  |   R  |   T  |  `   |           |  '   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -26,37 +26,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  ~L1 | LGui | LAlt | ESC  | LANG1|                                      |   `  |   =  |  [   |   ]  | ~L1  |
+ *   |  ~L1 |AltTAB| LGui | LAlt | LANG1|                                      |LANG2 |   =  |  [   |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        | PgDn | PgUp |       | DEL  |Ctrl/Esc|
+ *                                        | PgDn | PgUp |       | HOME |   END  |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      |PrtScr|       | RCTL |        |      |
+ *                                 |      |      |PrtScr|       | Ins  |        |      |
  *                                 | Space| TAB  |------|       |------|  Backsp|Enter |
- *                                 |      |      |LANG2 |       |LANG1 |  ace   |      |
+ *                                 |      |      |      |       |      |  ace   |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,         KC_2,     KC_3,    KC_4,   KC_5,   KC_GRV,
+        KC_ESC,         KC_1,         KC_2,     KC_3,    KC_4,   KC_5,   KC_6,
         KC_TAB,         KC_Q,         KC_W,     KC_E,    KC_R,   KC_T,   KC_GRV,
         KC_LCTRL,       KC_A,         KC_S,     KC_D,    KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,     KC_C,    KC_V,   KC_B,   KC_LBRC,
-        MO(SYMB),       KC_LGUI,      KC_LALT,  KC_ESC, KC_LANG1,
+        MO(SYMB),       LALT(KC_TAB), KC_LGUI,  KC_LALT,  KC_LANG1,
                                                 KC_PGDN, KC_PGUP,
                                                              KC_PSCREEN,
-                                                KC_SPC, KC_TAB, KC_LANG2,
+                                                KC_SPC, KC_TAB, KC_NO,
         // right hand
-        KC_6,        KC_7,   KC_8,   KC_9,     KC_0,    KC_MINS,    KC_EQL,
-        KC_QUOT,     KC_Y,   KC_U,   KC_I,     KC_O,    KC_P,       KC_BSLS,
-                     KC_H,   KC_J,   KC_K,     KC_L,    KC_SCLN,    KC_QUOT,
-        KC_RBRC,     KC_N,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,    KC_RSFT,
-                             KC_GRV, KC_EQL,   KC_LBRC, KC_RBRC,    MO(SYMB),
-        KC_DEL,        CTL_T(KC_ESC),
-        KC_RCTRL,
-        KC_LANG1,KC_BSPC,KC_ENT
+        KC_7,        KC_8,   KC_9,     KC_0,     KC_MINS, KC_EQL,     KC_BSPC,
+        KC_QUOT,     KC_Y,   KC_U,     KC_I,     KC_O,    KC_P,       KC_BSLS,
+                     KC_H,   KC_J,     KC_K,     KC_L,    KC_SCLN,    KC_QUOT,
+        KC_RBRC,     KC_N,   KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,    KC_RSFT,
+                             KC_LANG2, KC_EQL,   KC_LBRC, KC_RBRC,    MO(SYMB),
+        KC_HOME,    KC_END,
+        KC_INSERT,
+        KC_NO,KC_BSPC,KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -67,9 +67,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |VolDn |VolUp | Mute |      |      |------|           |------|      |      |      |  Up  |      |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |      | Prev | Next |      |      |      |           |      |      |      | LEFT |      |RIGHT |        |
+ * |         |      | Prev | Next |      |      |      |           |      |      | PGUP | LEFT |      |RIGHT |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |       |      |      |      |      |                                       |      |      | Down |LShift|      |
+ *   |       |      |      |      |      |                                       | PGDN |      | Down |LShift|      |
  *   `-----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |Animat|      |       |Toggle|Solid |
@@ -94,8 +94,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                 KC_TRNS, KC_TRNS,  KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS,  KC_LEFT, KC_NO,   KC_RGHT, KC_TRNS,
-                         KC_TRNS,  KC_TRNS, KC_DOWN, KC_LSFT, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_PGUP,  KC_LEFT, KC_NO,   KC_RGHT, KC_TRNS,
+                         KC_PGDN,  KC_TRNS, KC_DOWN, KC_LSFT, KC_TRNS,
        RGB_TOG, RGB_SLD,
        RGB_HUI,
        RGB_HUD, KC_DEL,  KC_INSERT
